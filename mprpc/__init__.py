e1b8e1c397827421b8e2f797155f05b4b17d7ce9
Wilfredo Lora <71466458+WilfredoLora@users.noreply.github.com>
2025-05-08 00:40:28 +0930

    Update __init__.py
    Not defining logger here anymore.

--- mprpc/__init__.py
+++ mprpc/__init__.py
@@ -1,12 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import logging
-from logging import NullHandler
-
 from mprpc.client import RPCClient, RPCPoolClient
 from mprpc.server import RPCServer
-
-logger = logging.getLogger(__name__)
-logger.addHandler(NullHandler())
 
 __all__ = ["RPCClient", "RPCPoolClient", "RPCServer"]
