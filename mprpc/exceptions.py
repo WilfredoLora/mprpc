fe09c3cc9f552f92be119bc0ad99e1baf931d1c9
Wilfredo Lora <71466458+WilfredoLora@users.noreply.github.com>
2025-05-07 21:55:24 +0930

    Update exceptions.py

--- mprpc/exceptions.py
+++ mprpc/exceptions.py
@@ -1,13 +1,11 @@
-# -*- coding: utf-8 -*-
-
-
 class RPCProtocolError(Exception):
+    """Raised when there is a protocol error in the RPC communication."""
     pass
-
 
 class MethodNotFoundError(Exception):
+    """Raised when the requested RPC method is not found."""
     pass
 
-
 class RPCError(Exception):
+    """Raised for general RPC-related errors."""
     pass
